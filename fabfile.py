bd639ccaaa2f7b7c354ef571d6edc3fae3809998
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2019-02-24 14:18:18 -0500

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,46 @@
+import os
+
+import boto3
+from fabric import task
+
+
+ROGUE_PATH = os.path.join(os.path.dirname(__file__))
+ROGUE_WEB_PATH = os.path.join(ROGUE_PATH, "web")
+ROGUE_BUILD_PATH = os.path.join(ROGUE_WEB_PATH, "build")
+ROGUE_WEB_HOST = "rogue.quuux.org"
+
+
+def deploy_website(path, bucket_name):
+    s3 = boto3.client('s3')
+
+    def upload(dirname, filename):
+
+        if filename[0] == "." or not os.path.isfile(os.path.join(dirname, filename)):
+            return
+
+        local_path = os.path.join(dirname, filename)
+        remote_path = local_path[len(path) + 1:]
+
+        print(local_path, '->', remote_path)
+        with open(local_path) as f:
+            s3.put_object(
+                Body=f.read(),
+                Bucket=bucket_name,
+                ACL="public-read",
+                Key=remote_path,
+            )
+
+    for dirname, _, filenames in os.walk(path):
+        for filename in filenames:
+            upload(dirname, filename)
+
+
+@task
+def build_web(c):
+    with c.cd(ROGUE_WEB_PATH):
+        c.local("PATH=\"$PATH:/usr/local/bin\" yarn build")
+
+
+@task
+def deploy_web(_):
+    deploy_website(ROGUE_BUILD_PATH, ROGUE_WEB_HOST)
